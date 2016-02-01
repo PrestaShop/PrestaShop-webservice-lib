eece6d40ef82b1838e9696354bf2d86bc0ef1fe7
nabahat <nabahatkhan@gmail.com>
2016-02-01 15:31:32 +0530

    Updating

--- examples/Delete.php
+++ examples/Delete.php
@@ -1,7 +1,7 @@
 <html><head><title>CRUD Tutorial - Delete example</title></head><body>
 <?php
 /*
-* 2007-2013 PrestaShop
+* 2007-2016 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -20,7 +20,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2013 PrestaShop SA
+*  @copyright  2007-2016 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 * PrestaShop Webservice Library
@@ -78,6 +78,9 @@ else
 		else echo 'Other error';
 	}
 
+	// UI
+
+	// We set the Title
 	echo '<h1>Customers List</h1>';
 	echo '<table border="5">';
 	if (isset($resources))
