c2c20d8acfbf350d876a806748ca3067cef01fbf
nabahat <nabahatkhan@gmail.com>
2016-02-01 15:31:30 +0530

    Updating

--- examples/Update.php
+++ examples/Update.php
@@ -1,7 +1,7 @@
 <html><head><title>CRUD Tutorial - Update example</title></head><body>
 <?php
 /*
-* 2007-2013 PrestaShop
+* 2007-2016 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -20,7 +20,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2013 PrestaShop SA
+*  @copyright  2007-2016 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 * PrestaShop Webservice Library
