2131ad7733b2a27dca5fb14b29e1542f0d30b621
Piyush Pangtey <piyush@newgenpayments.com>
2017-08-21 17:12:03 +0530

    Trim webservice's response before XML parsing.
    In order to avoid errors like 'XML declaration allowed only at the start of the
    document', trim trailing whitespaces is necessary.
    
    Signed-off-by: Piyush Pangtey <piyush@newgenpayments.com>

--- PSWebServiceLibrary.php
+++ PSWebServiceLibrary.php
@@ -199,7 +199,7 @@ class PrestaShopWebservice
 		{
 			libxml_clear_errors();
 			libxml_use_internal_errors(true);
-			$xml = simplexml_load_string($response,'SimpleXMLElement', LIBXML_NOCDATA);
+			$xml = simplexml_load_string(trim($response),'SimpleXMLElement', LIBXML_NOCDATA);
 			if (libxml_get_errors())
 			{
 				$msg = var_export(libxml_get_errors(), true);
