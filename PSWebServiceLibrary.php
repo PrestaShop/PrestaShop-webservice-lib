988fafd05be626e03768a4068b6e0d3c01da831f
Radosław Kozioł <radoslaw.koziol@polcode.net>
2017-10-19 12:15:47 +0200

    Allow filter by date_add && date_upd
    $options['date'] = 1; has to be added to allow user sorting by date_add and date_upd. Without 'date' it's impossible.

--- PSWebServiceLibrary.php
+++ PSWebServiceLibrary.php
@@ -343,7 +343,7 @@ class PrestaShopWebservice
                 $url .= '/' . $options['id'];
             }
 
-            $params = array('filter', 'display', 'sort', 'limit', 'id_shop', 'id_group_shop', 'schema', 'language');
+            $params = array('filter', 'display', 'sort', 'limit', 'id_shop', 'id_group_shop', 'schema', 'language', 'date');
             foreach ($params as $p) {
                 foreach ($options as $k => $o) {
                     if (strpos($k, $p) !== false) {
