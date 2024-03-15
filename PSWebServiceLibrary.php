826c3ca0c2270fddb4c6fcbf1314c8009325bf6f
Hervé H <nenes25@users.noreply.github.com>
2024-03-15 09:08:21 +0100

    Increase max Ps version

--- PSWebServiceLibrary.php
+++ PSWebServiceLibrary.php
@@ -47,7 +47,7 @@ class PrestaShopWebservice
     /** @var string Minimal version of PrestaShop to use with this library */
     const psCompatibleVersionsMin = '1.4.0.0';
     /** @var string Maximal version of PrestaShop to use with this library */
-    const psCompatibleVersionsMax = '8.1.1';
+    const psCompatibleVersionsMax = '8.1.4';
 
     /**
      * PrestaShopWebservice constructor. Throw an exception when CURL is not installed/activated
